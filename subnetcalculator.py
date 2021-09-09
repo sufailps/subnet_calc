621f7b92a839a8c8cccdf4463f313ee2ebbca9d6
sufailps <67498736+sufailps@users.noreply.github.com>
2021-09-09 09:29:57 +0530

    Add files via upload

---
+++ subnetcalculator.py
@@ -0,0 +1,65 @@
+import re
+from tkinter import *
+win= Tk()
+win.geometry("600x450")
+
+def main_fun():
+    ipaddress = ip.get()
+    subnet=int(sub.get())
+    iplist = list(ipaddress.split("."))
+    maskaddress = str("1"*subnet+"0"*(32-subnet)) 
+    netmask1 = [int(maskaddress[i:i+8],2) for i in range(0,len(maskaddress),8)]
+    print("netmask",netmask1)
+    subnet_mask_ipv4 =".".join(map(str, netmask1))
+    network_address= [int(iplist[i])&netmask1[i] for i in range(4)]
+    n =".".join(map(str, network_address))
+    #broad cast
+    inverted_subnet=''.join(str("0"*subnet+"1"*(32-subnet)))
+    inverted= re.findall('........',inverted_subnet)
+    invert_lst = []
+    for i in inverted:
+        invert_lst.append(int(i,2)) #convert to decimal
+    
+    brodcast_lst = []
+    for i in range(len(iplist)):
+        brodcast_lst.append(network_address[i] | invert_lst[i]) #decimal version  (OR)
+    brodacast_address = ".".join(map(str, brodcast_lst))
+    #to find no of ip address   = 2^n
+    no_of_ip= pow(2, 32-subnet)
+    usable = no_of_ip - 2
+    network_address[3] = network_address[3] + 1
+    start_range = ".".join(map(str, network_address))
+    brodcast_lst[3] = int(brodcast_lst[3]) - 1
+    end_range = ".".join(map(str, brodcast_lst))
+    iprange = start_range + " - " + end_range
+    #to print     
+    Label(win, text="IP address").grid(row=31,column=1)
+    Label(win, text=ipaddress).grid(row=31,column=2)
+    Label(win, text="Subnet mask").grid(row=32,column=1)
+    Label(win, text=subnet_mask_ipv4).grid(row=32,column=2)
+    Label(win, text="Network address").grid(row=33,column=1)
+    Label(win, text=n).grid(row=33,column=2)
+    Label(win, text="Broadcast address").grid(row=34,column=1)
+    Label(win, text=brodacast_address).grid(row=34,column=2)
+    Label(win, text="Available IP addresses:").grid(row=35,column=1)
+    Label(win, text=usable).grid(row=35,column=2)
+    Label(win, text="Range ").grid(row=36,column=1)
+    Label(win, text=iprange).grid(row=36,column=2)
+
+def clear():
+	ip.set('')
+    
+	sub.set('')	
+#main 
+win.title('Subnet Calculator')
+Label(win,text="Subnet calculator      ").grid(row=1,column=0)
+Label(win, text="Enter the ip address  ").grid(row=10,column=0)
+Label(win, text="Enter the subnetmask  ").grid(row=11,column=0)
+ip = StringVar(win)
+sub=StringVar(win)
+ip_addr = Entry(win, textvariable=ip).grid(row=10,column=1)
+sub_net_mask = Entry(win,textvariable=sub).grid(row=11,column=1)
+Label(win,text="").grid(row=12,column=0)
+Button(win,text="Calculate",command=main_fun).grid(row=20,column=1)
+Button(win,text="clear",command=clear).grid(row=20,column=3)
+win.mainloop()
